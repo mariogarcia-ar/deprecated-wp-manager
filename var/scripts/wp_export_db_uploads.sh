9bf9d3ed31d3b0f624448321aff25c6ce8c6b322
Mario Garcia <mariogarcia.ar@gmail.com>
2024-04-17 16:02:17 -0300

    add plugins, theme, options in the exportation

--- var/scripts/wp_export_db_uploads.sh
+++ var/scripts/wp_export_db_uploads.sh
@@ -1,13 +1,48 @@
 #!/bin/bash
 
+# fix for php7
+cd ~/public_html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+# myphp="/usr/local/lsws/lsphp80/bin/php -d memory_limit=512M -d error_reporting=E_ALL\&~E_WARNING"
+# mywp="/usr/local/lsws/lsphp80/bin/php -d memory_limit=512M ~/public_html/wp-cli.phar plugin list  --format=csv"
+mywp="/usr/local/lsws/lsphp80/bin/php -d memory_limit=512M /home/amestienda.com.ar/wp-cli.phar "
+
+
 # This script is used to backup the database and uploads folder of a WordPress site.
 
 # Create a directory to store the backups
+cd ~
+
 mkdir -p ~/backup-prod/
 
 # Backup the database
 cd ~/public_html
-wp db export ~/backup-prod/amestienda.com.ar.sql
+
+echo "ames.tienda.com.ar" > ~/backup-prod/Readme.txt
+
+wp db export ~/backup-prod/database.sql
+
+
+
+$mywp plugin list  --format=csv > ~/backup-prod/plugins.csv
+$mywp theme list  --format=csv > ~/backup-prod/themes.csv
+
+$mywp  option list --format=csv > ~/backup-prod/options.csv
+
+$mywp post-type list --format=csv > ~/backup-prod/post-types.csv
+$mywp user list --format=csv > ~/backup-prod/users.csv
+$mywp user meta list --format=csv > ~/backup-prod/user-meta.csv
+$mywp term list --format=csv > ~/backup-prod/terms.csv
+$mywp term meta list --format=csv > ~/backup-prod/term-meta.csv
+$mywp taxonomy list --format=csv > ~/backup-prod/taxonomies.csv
+$mywp taxonomy meta list --format=csv > ~/backup-prod/taxonomy-meta.csv
+$mywp role list --format=csv > ~/backup-prod/roles.csv
+$mywp capability list --format=csv > ~/backup-prod/capabilities.csv
+$mywp sidebar list --format=csv > ~/backup-prod/sidebars.csv
+$mywp menu list --format=csv > ~/backup-prod/menus.csv
+$mywp menu location list --format=csv > ~/backup-prod/menu-locations.csv
+$mywp widget list --format=csv > ~/backup-prod/widgets.csv
+
 
 # Backup the uploads folder
 cd ~/public_html/wp-content/
@@ -15,10 +50,11 @@ find ./uploads -type f |  grep  -v -E '\-[[:digit:]]{1,4}x[[:digit:]]{1,4}' | ta
 
 # Compress the backup
 cd ~
-zip -r backup-prod.zip ~/backup-prod/
+tar -czf backup-prod.tar.gz -C ~/backup-prod/ .
+
 rm -rf ~/backup-prod/
 
 # Copy the backup to a remote server
-LOCAL_DIR="./backup-prod.zip"
+LOCAL_DIR="./backup-prod.tar.gz"
 REMOTE_DIR="dw_prodtco:~/amestienda.com.ar/"
 rsync -avz $LOCAL_DIR $REMOTE_DIR
