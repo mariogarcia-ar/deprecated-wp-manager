860a03fa1285387af0826f4c4253377840fbb842
Mario Garcia <mariogarcia.ar@gmail.com>
2024-04-17 11:37:42 -0300

    add prune

--- var/lib/pw_manager_docker.sh
+++ var/lib/pw_manager_docker.sh
@@ -10,3 +10,16 @@ docker_composer_down() {
     echo "Stoping services... $1"
     $docker_composer down $1
 }
+
+docker_prune() {
+    docker_composer_down $1
+
+    # TODO: add a user confirmation
+    echo "Removing volumes and temp directories..."
+    sudo rm -rf $PW_VOLUMES_DIR
+    sudo rm -rf $PW_TMP_DIR
+
+    echo "Creating directories ..."
+    mkdir -p $PW_VOLUMES_DIR
+    mkdir -p $PW_TMP_DIR
+}
